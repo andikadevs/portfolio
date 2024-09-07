48f8fb52bb065ed566563e0b01c2f057f5eada7d
Andikss <andikadwisaputra.dev@gmail.com>
2024-09-07 15:01:14 +0700

    feat: display skills

--- src/sections/Home/Skills/Skills.tsx
+++ src/sections/Home/Skills/Skills.tsx
@@ -1,20 +1,28 @@
+'use client';
+
+import { useState } from 'react';
 import { Title } from '@/components/Global';
 import Image from 'next/image';
 import React from 'react';
 import skillsData from './skills.json'; 
 import { Skill } from './Skills';
+import { MdArrowUpward, MdArrowDownward } from 'react-icons/md';
 
 const skills: Skill[] = skillsData;
 
 export const Skills: React.FC = () => {
+  const [showAll, setShowAll] = useState(false);
+
+  const displayedSkills = showAll ? skills : skills.slice(0, 12);
+
   return (
     <div className='bg-main h-auto w-full pb-20'>
       <Title
         title='Skills'
         description='Toolkits that help me [complete] all of my [projects]'
       />
-      <div className='grid gap-4 px-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6'>
-        {skills.map((skill, index) => (
+      <div className={`grid gap-4 px-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 transition-all duration-500 ease-in-out overflow-hidden'}`}>
+        {displayedSkills.map((skill, index) => (
           <a
             key={index}
             href={skill.url}
@@ -27,6 +35,24 @@ export const Skills: React.FC = () => {
           </a>
         ))}
       </div>
+      {skills.length > 12 && (
+        <div className='text-center mt-8 flex justify-center'>
+          <button
+            onClick={() => setShowAll(!showAll)}
+            className='bg-secondary text-accent px-6 py-3 rounded shadow-lg hover:text-secondary hover:bg-accent transition duration-300 ease-in-out flex items-center justify-center'
+          >
+            {showAll ? (
+              <>
+                <MdArrowUpward className='mr-2' /> Show Less
+              </>
+            ) : (
+              <>
+                <MdArrowDownward className='mr-2' /> Show More
+              </>
+            )}
+          </button>
+        </div>
+      )}
     </div>
   );
 };
