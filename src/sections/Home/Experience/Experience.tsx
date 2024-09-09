3c213ec02085c837123f505c676c1e0375870522
Andikss <andikadwisaputra.dev@gmail.com>
2024-09-09 14:05:31 +0700

    update: completly delete icon from experience on mobile

--- src/sections/Home/Experience/Experience.tsx
+++ src/sections/Home/Experience/Experience.tsx
@@ -56,7 +56,7 @@ export const Experience: React.FC = () => {
   }, []);
 
   return (
-    <div className='h-auto w-full p-12 bg-secondary shadow-xl relative mb-[60px]'>
+    <div className='h-auto w-full px-4 md:px-12 pt-12 bg-secondary shadow-xl relative mb-[60px]'>
       <h3 className="text-text text-4xl absolute top-[-20px] left-4">
         Experience
       </h3>
@@ -100,17 +100,6 @@ export const Experience: React.FC = () => {
             </div>
           </div>
         </>
-      )}
-
-      {/* Single Icon Display on Mobile and Tablet */}
-      {isMobileOrTablet && (
-        <div className="md:hidden flex justify-center">
-          <img 
-            src="assets/static/img/Icons/csharp.svg" 
-            alt="C#" 
-            className='h-[100px]'
-          />
-        </div>
       )}
     </div>
   );
