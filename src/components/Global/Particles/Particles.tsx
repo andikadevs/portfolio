25dff3105e0fd550a0c9b1570257676e8018fabe
Andikss <andikadwisaputra.dev@gmail.com>
2024-09-10 14:18:41 +0700

    fix: particles

--- src/components/Global/Particles/Particles.tsx
+++ src/components/Global/Particles/Particles.tsx
@@ -1,11 +1,11 @@
 'use client'
-/* eslint-disable */
-import ReactParticles, { initParticlesEngine } from "@tsparticles/react";
-import { useEffect, useMemo, useState } from "react";
+
+import ReactParticles , { initParticlesEngine } from "@tsparticles/react";
+import { useEffect, useState } from "react";
 import { loadSlim } from "@tsparticles/slim";
-import { Engine } from "@tsparticles/engine";
 
 export const Particles = (props: { id: string | undefined; }) => {
+
   const [init, setInit] = useState(false);
   console.log(init);
 
@@ -17,82 +17,71 @@ export const Particles = (props: { id: string | undefined; }) => {
     });
   }, []);
 
-  const particlesLoaded = (container: Engine) => {
-    console.log(container);
-  };
-
-  const options = useMemo(
-    () => ({
-      interactivity: {
-        events: {
-          onClick: {
-            enable: false,
-            mode: 'push',
-          },
-          onHover: {
-            enable: true,
-            mode: 'repulse',
-          },
-          resize: {
-            enable: true, 
-          },
+  return <ReactParticles id={props.id} options={{
+    interactivity: {
+      events: {
+        onClick: {
+          enable: false,
+          mode: 'push',
         },
-        modes: {
-          push: {
-            quantity: 90,
-          },
-          repulse: {
-            distance: 200,
-            duration: 0.4,
-          },
+        onHover: {
+          enable: true,
+          mode: 'repulse',
+        },
+        resize: {
+          enable: true, 
         },
       },
-      particles: {
-        color: {
-          value: '#e68e2e',
+      modes: {
+        push: {
+          quantity: 90,
         },
-        links: {
-          color: '#f5d393',
-          distance: 150,
-          enable: true,
-          opacity: 0.5,
-          width: 1,
-        },
-        collisions: {
-          enable: true,
-        },
-        move: {
-          direction: 'none',
-          enable: true,
-          outModes: {
-            default: 'bounce',
-          },
-          random: false,
-          speed: 1,
-          straight: false,
-        },
-        number: {
-          density: {
-            enable: true,
-            area: 800,
-          },
-          value: 80,
-        },
-        opacity: {
-          value: 0.5,
-        },
-        shape: {
-          type: 'circle',
-        },
-        size: {
-          value: { min: 1, max: 5 },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
         },
       },
-      detectRetina: true,
-    }),
-    [],
-  );
-
-  return <ReactParticles id={props.id} init={particlesLoaded} options={options} />;
+    },
+    particles: {
+      color: {
+        value: '#e68e2e',
+      },
+      links: {
+        color: '#f5d393',
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      collisions: {
+        enable: true,
+      },
+      move: {
+        direction: 'none',
+        enable: true,
+        outModes: {
+          default: 'bounce',
+        },
+        random: false,
+        speed: 1,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+        },
+        value: 80,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: 'circle',
+      },
+      size: {
+        value: { min: 1, max: 5 },
+      },
+    },
+    detectRetina: true,
+  }} />; 
 };
-/* eslint-enable */
