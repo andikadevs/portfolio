1bc7c85376aeac3649794c10dc8cb36525283a67
Andikss <andika.dwi.saputra.7925@gmail.com>
2024-09-11 11:08:54 +0700

    feat: add avatar on homepage

--- src/sections/Home/Hero/Hero.tsx
+++ src/sections/Home/Hero/Hero.tsx
@@ -1,28 +1,31 @@
 import { SosmedChain } from '@/components/Home';
 import { Particles, Typing } from '@/components/Global'; 
+import { FaChrome } from 'react-icons/fa';
 
 export const Hero = () => {
   const whatIam = ['Web Developer', 'Software Engineer', 'Determined Learner'];
 
   return (
-    <div id='home' className='relative bg-main h-screen w-full'>
+    <div id='home' className='relative bg-main h-[100dvh] w-full'>
 
       <Particles id="particles"/>
       {/* <HeroImage /> */}
       <SosmedChain />
 
       <div className="relative h-screen grid grid-cols-1 md:grid-cols-2 w-full z-[1]">
-        <div className='pl-12 flex w-full flex-col items-start justify-center'>
-          <h1 className='text-center text-accent text-3xl md:text-4xl lg:text-5xl border-b-2 border-accent'>
-            Andika Dwi Saputra
-          </h1>
-          <h2 className='text-center text-accent text-xl'>
-            {`I’m `}
-            <Typing texts={whatIam} />
-          </h2>
+        <div className='flex w-full flex-col md:justify-center md:pl-14 items-center justify-end'>
+          <div className="flex justify-center flex-col">
+            <h1 className='flex items-center gap-2 pb-1 text-center text-accent whitespace-nowrap text-3xl md:text-4xl lg:text-5xl border-b-2 border-accent'>
+              <FaChrome className='text-accent'/> Fullstack Developer
+            </h1>
+            <h2 className='text-start text-accent text-xl'>
+              {`I’m `}
+              <Typing texts={whatIam} />
+            </h2>
+          </div>
         </div>
-        <div className='p-4'>
-
+        <div>
+          <img className='hidden md:block absolute bottom-0 h-[70%] md:right-8 xl:right-12 xl:h-[80%] object-contain' src="/assets/static/img/Avatar.webp" alt="Avatar" />
         </div>
       </div>
     </div>
