5c0ad43f77a8d9234239911162d203b7b1ce405c
Andikss <andika.dwi.saputra.7925@gmail.com>
2024-09-11 13:59:55 +0700

    fix: company logo height

--- src/sections/Home/Companies/Compaines.tsx
+++ src/sections/Home/Companies/Compaines.tsx
@@ -8,24 +8,24 @@ export const Compaines = () => {
       </p>
       <div className='flex space-x-16 overflow-hidden bg-secondary'>
         <div className="flex space-x-16 animate-loop-scroll items-center">
-          <img src="/assets/static/img/Companies/Akastra.png" alt="Akastra Toyota" className="max-w-none grayscale hover:grayscale-0 h-12"/>
-          <img src="/assets/static/img/Companies/Toploker.png" alt="Toploker.com" className="max-w-none grayscale hover:grayscale-0 h-12"/>
-          <img src="/assets/static/img/Companies/DIGITEK.png" alt="PT Digital Bisnis Ekonomi" className="max-w-none grayscale hover:grayscale-0 h-40"/>
-          <img src="/assets/static/img/Companies/STEKOM.png" alt="Stekom University" className="max-w-none grayscale hover:grayscale-0 h-12"/>
-          <img src="/assets/static/img/Companies/Akastra.png" alt="Akastra Toyota" className="max-w-none grayscale hover:grayscale-0 h-12"/>
-          <img src="/assets/static/img/Companies/Toploker.png" alt="Toploker.com" className="max-w-none grayscale hover:grayscale-0 h-12"/>
-          <img src="/assets/static/img/Companies/DIGITEK.png" alt="PT Digital Bisnis Ekonomi" className="max-w-none grayscale hover:grayscale-0 h-40"/>
-          <img src="/assets/static/img/Companies/STEKOM.png" alt="Stekom University" className="max-w-none grayscale hover:grayscale-0 h-12"/>
+          <img src="/assets/static/img/Companies/Akastra.png" alt="Akastra Toyota" className="max-w-none grayscale hover:grayscale-0 h-10"/>
+          <img src="/assets/static/img/Companies/Toploker.png" alt="Toploker.com" className="max-w-none grayscale hover:grayscale-0 h-10"/>
+          <img src="/assets/static/img/Companies/DIGITEK.png" alt="PT Digital Bisnis Ekonomi" className="max-w-none grayscale hover:grayscale-0 h-36"/>
+          <img src="/assets/static/img/Companies/STEKOM.png" alt="Stekom University" className="max-w-none grayscale hover:grayscale-0 h-10"/>
+          <img src="/assets/static/img/Companies/Akastra.png" alt="Akastra Toyota" className="max-w-none grayscale hover:grayscale-0 h-10"/>
+          <img src="/assets/static/img/Companies/Toploker.png" alt="Toploker.com" className="max-w-none grayscale hover:grayscale-0 h-10"/>
+          <img src="/assets/static/img/Companies/DIGITEK.png" alt="PT Digital Bisnis Ekonomi" className="max-w-none grayscale hover:grayscale-0 h-36"/>
+          <img src="/assets/static/img/Companies/STEKOM.png" alt="Stekom University" className="max-w-none grayscale hover:grayscale-0 h-10"/>
         </div>
         <div className="flex space-x-16 animate-loop-scroll items-center" aria-hidden="true">
-          <img src="/assets/static/img/Companies/Akastra.png" alt="Akastra Toyota" className="max-w-none grayscale hover:grayscale-0 h-12"/>
-          <img src="/assets/static/img/Companies/Toploker.png" alt="Toploker.com" className="max-w-none grayscale hover:grayscale-0 h-12"/>
-          <img src="/assets/static/img/Companies/DIGITEK.png" alt="PT Digital Bisnis Ekonomi" className="max-w-none grayscale hover:grayscale-0 h-40"/>
-          <img src="/assets/static/img/Companies/STEKOM.png" alt="Stekom University" className="max-w-none grayscale hover:grayscale-0 h-12"/>
-          <img src="/assets/static/img/Companies/Akastra.png" alt="Akastra Toyota" className="max-w-none grayscale hover:grayscale-0 h-12"/>
-          <img src="/assets/static/img/Companies/Toploker.png" alt="Toploker.com" className="max-w-none grayscale hover:grayscale-0 h-12"/>
-          <img src="/assets/static/img/Companies/DIGITEK.png" alt="PT Digital Bisnis Ekonomi" className="max-w-none grayscale hover:grayscale-0 h-40"/>
-          <img src="/assets/static/img/Companies/STEKOM.png" alt="Stekom University" className="max-w-none grayscale hover:grayscale-0 h-12"/>
+          <img src="/assets/static/img/Companies/Akastra.png" alt="Akastra Toyota" className="max-w-none grayscale hover:grayscale-0 h-10"/>
+          <img src="/assets/static/img/Companies/Toploker.png" alt="Toploker.com" className="max-w-none grayscale hover:grayscale-0 h-10"/>
+          <img src="/assets/static/img/Companies/DIGITEK.png" alt="PT Digital Bisnis Ekonomi" className="max-w-none grayscale hover:grayscale-0 h-36"/>
+          <img src="/assets/static/img/Companies/STEKOM.png" alt="Stekom University" className="max-w-none grayscale hover:grayscale-0 h-10"/>
+          <img src="/assets/static/img/Companies/Akastra.png" alt="Akastra Toyota" className="max-w-none grayscale hover:grayscale-0 h-10"/>
+          <img src="/assets/static/img/Companies/Toploker.png" alt="Toploker.com" className="max-w-none grayscale hover:grayscale-0 h-10"/>
+          <img src="/assets/static/img/Companies/DIGITEK.png" alt="PT Digital Bisnis Ekonomi" className="max-w-none grayscale hover:grayscale-0 h-36"/>
+          <img src="/assets/static/img/Companies/STEKOM.png" alt="Stekom University" className="max-w-none grayscale hover:grayscale-0 h-10"/>
         </div>
       </div>
     </div>
