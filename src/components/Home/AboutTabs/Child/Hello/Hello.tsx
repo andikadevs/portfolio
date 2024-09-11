94b760fd39b06554b5cadb49a13cf15cead241b2
Andikss <andika.dwi.saputra.7925@gmail.com>
2024-09-11 14:48:46 +0700

    feat: download cv

--- src/components/Home/AboutTabs/Child/Hello/Hello.tsx
+++ src/components/Home/AboutTabs/Child/Hello/Hello.tsx
@@ -1,26 +1,40 @@
-import { Button } from "@/components/Global"
-import { FaChrome, FaDownload, FaWhatsapp } from "react-icons/fa"
+import React from 'react';
+import { Button, Tooltip } from '@/components/Global';
+import { FaChrome, FaDownload, FaWhatsapp } from 'react-icons/fa';
 
-export const Hello = () => {
+export const Hello: React.FC = () => {
+  const handleDownload = () => {
+    const link    = document.createElement('a');
+    link.href     = '/assets/static/data/curriculum-vitae.pdf'; 
+    link.download = 'curicullum-vitae.pdf';
+    link.click();
+  };
+
   return (
     <div className="min-h-[400px] flex justify-center flex-col">
       <h3 className="flex items-center gap-2 text-text text-3xl mb-4">
-        <FaChrome/> Fullstack <span className="text-accent">Developer</span>
+        <FaChrome /> Fullstack <span className="text-accent">Developer</span>
       </h3>
 
       <p className="text-text mb-5">
-        Hello there! I`m Andika Dwi Saputra, and I`m a <span className="text-accent">Fullstack Developer!</span> Feel free to check out my portfolio!
+        Hello there! I’m Andika Dwi Saputra, and I’m a <span className="text-accent">Fullstack Developer!</span> Feel free to check out my portfolio!
       </p>
 
       <div className="flex gap-3">
-        <Button variant="outline">
-          <FaDownload/>Download CV
-        </Button>
+        <Tooltip hasArrow position='bottom' label='Download my CV'>
+          <Button variant="outline" onClick={handleDownload}>
+            <FaDownload /> Download CV
+          </Button>
+        </Tooltip>
 
-        <Button variant="fill">
-          <FaWhatsapp/> Contact Me
-        </Button>
+        <a href="https://wa.me/6285743699909">
+          <Tooltip hasArrow position='bottom' label='Chat me on WhatsApp!'>
+            <Button variant="fill">
+              <FaWhatsapp /> Contact Me
+            </Button>
+          </Tooltip>
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
