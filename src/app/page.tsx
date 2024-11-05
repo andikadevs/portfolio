828673864138b99caf51541a64c8aadd5f0d2dc8
Andikss <andikadwisaputra.dev@gmail.com>
2024-11-05 14:15:30 +0700

    update: trial remove particles(all)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { Navbar, Facts, Footer, Particles } from "@/components/Global";
+import { Navbar, Facts, Footer } from "@/components/Global";
 import { About, Certificate, Companies, Education, Experience, Hero, Portfolio, Skills } from "@/sections/Home";
 
 export default function Home() {
   return (
     <main className="bg-main relative">
-      <Particles id="particles" />
       <Navbar />
       <Facts />
 
