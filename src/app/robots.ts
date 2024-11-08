46f2007fcb44638d66411cc245de60ef75d6225e
Andikss <andika.dwi.saputra.7925@gmail.com>
2024-11-08 20:41:31 +0700

    update: robors

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+const baseUrl = 'https://andikads.my.id'
+
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*'
+      }
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl
+  };
+}
