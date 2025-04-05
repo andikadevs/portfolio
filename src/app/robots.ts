f52b64e4ab9bdb092d3139f98198f284c361b515
Andikss <andika.dwi.saputra.7925@gmail.com>
2025-04-05 22:25:49 +0700

    fix: robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from 'next';
 
-export function GET(): MetadataRoute.Robots {
+export default function robots(): MetadataRoute.Robots {
   const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://andikads.cloud';
   
   return {
