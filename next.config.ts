e45e19cf5aea456c14a68b01eddd637e97d112d8
Andikss <andika.dwi.saputra.7925@gmail.com>
2025-12-27 15:17:27 +0700

    migrate to bun and upgrade nextjs and react version

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "images.unsplash.com",
-      "images.pexels.com",  
-      "images.pexels.co",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.co",
+      },
     ],
   },
 };
